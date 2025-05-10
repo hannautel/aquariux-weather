90e9f990865f58d50bf80dd52b9a81dcbb3d3911
Clement Le <tuananhit.oct@gmail.com>
2025-05-10 18:18:28 +0700

    fix: align padding of main container

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="w-full h-full">
       <Header />
       <main className="pt-[60px]">
-        <div className="max-w-2xl xs:w-full sm:w-md mx-auto p-4 space-y-4">
+        <div className="max-w-2xl xs:w-full sm:w-md mx-auto p-4">
           <Routes>
             <Route index element={<Weather />} />
             <Route path="search" element={<WeatherSeach />} />
