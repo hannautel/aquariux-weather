da4eff649f99e82831bfed0fccdf196a48013495
Clement Le <tuananhit.oct@gmail.com>
2025-05-10 12:38:28 +0700

    feat: add current weather box to home page

--- src/modules/Weather/Weather.tsx
+++ src/modules/Weather/Weather.tsx
@@ -1,10 +1,7 @@
+import WeatherDetail from './parts/WeatherDetail';
+
 function Weather() {
-  return (
-    <div>
-      <h1>Weather Module</h1>
-      <p>This is the Weather module.</p>
-    </div>
-  );
+  return <WeatherDetail />;
 }
 
 export default Weather;
