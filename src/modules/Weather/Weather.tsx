abea926eb459a23798aebe02b440a892a76c8224
Clement Le <tuananhit.oct@gmail.com>
2025-05-10 18:19:40 +0700

    feat: adapt ui to weather page

--- src/modules/Weather/Weather.tsx
+++ src/modules/Weather/Weather.tsx
@@ -1,7 +1,15 @@
 import WeatherDetail from './parts/WeatherDetail';
+import WeatherForecast from './parts/WeatherForecast';
 
 function Weather() {
-  return <WeatherDetail />;
+  return (
+    <>
+      <WeatherDetail />
+      <div className="mt-6">
+        <WeatherForecast />
+      </div>
+    </>
+  );
 }
 
 export default Weather;
