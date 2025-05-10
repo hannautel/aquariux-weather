a998b8b5aaade0d8aaad0edfc6377b26c1d6f79a
Clement Le <tuananhit.oct@gmail.com>
2025-05-10 11:27:03 +0700

    feat: init route provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from 'react-router';
+import Weather from './modules/Weather';
+import WeatherSeach from './modules/WeatherSearch';
+
 function App() {
-  return <div>React App</div>;
+  return (
+    <div className="w-full h-full">
+      <Routes>
+        <Route index element={<Weather />} />
+        <Route path="search" element={<WeatherSeach />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/modules/Weather/Weather.tsx
@@ -0,0 +1,10 @@
+function Weather() {
+  return (
+    <div>
+      <h1>Weather Module</h1>
+      <p>This is the Weather module.</p>
+    </div>
+  );
+}
+
+export default Weather;
