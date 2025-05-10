f6bf5b146160afa3075eea8398cb99e4033b99a0
Clement Le <tuananhit.oct@gmail.com>
2025-05-10 11:34:53 +0700

    feat: add icon library (lucide)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router';
 import Weather from './modules/Weather';
 import WeatherSeach from './modules/WeatherSearch';
+import { MapPin } from 'lucide-react';
 
 function App() {
   return (
     <div className="w-full h-full">
+      <MapPin fontSize="20px" />
       <Routes>
         <Route index element={<Weather />} />
         <Route path="search" element={<WeatherSeach />} />
