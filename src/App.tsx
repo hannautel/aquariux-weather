9c3eb939c0ed7e29eb888cf023179270a3d7e12c
Clement Le <tuananhit.oct@gmail.com>
2025-05-10 12:34:34 +0700

    feat: adujst main section with header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Route, Routes } from 'react-router';
+import Header from './core/Header';
 import Weather from './modules/Weather';
 import WeatherSeach from './modules/WeatherSearch';
-import { MapPin } from 'lucide-react';
 
 function App() {
   return (
     <div className="w-full h-full">
-      <MapPin fontSize="20px" />
-      <Routes>
-        <Route index element={<Weather />} />
-        <Route path="search" element={<WeatherSeach />} />
-      </Routes>
+      <Header />
+      <main className="pt-[60px]">
+        <div className="max-w-2xl xs:w-full sm:w-md mx-auto p-4 space-y-4">
+          <Routes>
+            <Route index element={<Weather />} />
+            <Route path="search" element={<WeatherSeach />} />
+          </Routes>
+        </div>
+      </main>
     </div>
   );
 }
