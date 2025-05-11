88e42980e06a11f440c62d14f2eab1b39fe2ad4e
Clement Le <tuananhit.oct@gmail.com>
2025-05-11 15:37:45 +0700

    feat: add header

--- src/core/Header.tsx
+++ src/core/Header.tsx
@@ -1,16 +1,24 @@
+import { Link } from 'react-router';
 import { MapPin, SearchIcon } from 'lucide-react';
+import { useSelectedLocationContext } from '@contexts/SelectedLocationContext';
 
 function Header() {
+  const { coord } = useSelectedLocationContext();
+
   return (
     <header className="fixed top-0 left-0 w-full shadow bg-white">
-      <div className="max-w-2xl xs:w-full sm:w-md mx-auto p-4 space-y-4 flex items-center justify-between gap-2">
-        <div className="flex items-center gap-2 m-0">
-          <MapPin fontSize="20px" />
-          <h5 className="text-lg font-medium text-gray-900">Singapore, SG</h5>
-        </div>
-        <button className="leading-0" role="button">
+      <div className="max-w-2xl xs:w-full sm:w-md mx-auto p-4 flex items-center justify-between gap-2">
+        <Link to="/">
+          <div className="flex items-center gap-2 m-0">
+            <MapPin fontSize="20px" />
+            <h5 className="text-lg font-medium text-gray-900">
+              {coord.name}, {coord.country}
+            </h5>
+          </div>
+        </Link>
+        <Link to="/search">
           <SearchIcon fontSize="20px" />
-        </button>
+        </Link>
       </div>
     </header>
   );
